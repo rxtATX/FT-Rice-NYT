f5ae8518d39e7aeb75b9680dde4cc080d186c688
arthiatsr <arthiatsr@users.noreply.github.com>
2019-10-25 14:25:36 -0500

    third submit

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
 
     
-    $("#click-me").on("click", function() {
-      alert("I've been clicked!");
+    $("#searchbutton").on("click", function(event) {
+      event.preventDefault();
+    console.log("I am search");
+    var searchItem = $("#searchItem");
+    var searchtext = searchItem.val();
+    console.log(searchtext);
+    var numRec = $("#numRec");
+    var numtext = numRec.val();
+    console.log(numtext);
+    var startYear = $("#startYear");
+    var starttext = startYear.val();
+    console.log(starttext);
+    var endYear = $("#endYear");
+    var endtext = endYear.val();
+    console.log(endtext);
     });
 
   });
